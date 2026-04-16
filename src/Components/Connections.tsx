6bc512d631f87e5472e239a2097fd83e729834e3
VJAPP <vjaapp2026@gmail.com>
2026-04-16 10:04:44 -0300

    Update Connections.tsx

--- src/Components/Connections.tsx
+++ src/Components/Connections.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { styles } from '../styles'; // Importamos estilos generales
+import styles from '../styles'; // Importamos estilos generales
 
 export default function Connections({
   connections,
